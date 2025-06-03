66be7813bf71cd714c4c5af8ce4f2374ada22503
kotono <2713421980@qq.com>
2025-06-03 10:20:39 +0800

    Init .Net Wappi

---
+++ SubSystem/ClientSubSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace AK.Wwise.Waapi.SubSystem
+{
+
+    public class ClientSubSystem:Singleton<ClientSubSystem>
+    {
+        private AK.Wwise.Waapi.JsonClient client;
+        public AK.Wwise.Waapi.JsonClient Client => client;
+
+        public ClientSubSystem()
+        {
+           
+        }
+        
+        public void Initialization()
+        {
+            client = new AK.Wwise.Waapi.JsonClient();
+        }
+        
+        // Try to connect to running instance of Wwise on localhost, default port
+        public  async Task<bool> ConnectWwise()
+        {
+            await Client.Connect();
+            return true;
+        }
+    
+        public void SubscribeDisconnected(Wamp.DisconnectedHandler handler)
+            => Client.Disconnected += handler;
+
+    }
+}
